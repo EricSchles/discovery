3e6ebdb4ddd1af718c2b8bde6f050ae67bcdcbfa
Eric Schles <ericschles@gmail.com>
2016-10-05 17:09:55 -0400

    fixed fixture issue

--- vendors/global_replace.py
+++ vendors/global_replace.py
@@ -10,12 +10,13 @@ for py_file in glob("*"):
     with open(py_file,"r") as f:
         text = f.read()
     with open(py_file,"w") as f:
-        text = text.replace("vendorssss","vendors")
-        text = text.replace("vendorsss","vendors")
-        text = text.replace("vendorss","vendors")
-        text = text.replace("Vendorssss","Vendors")
-        text = text.replace("Vendorsss","Vendors")
-        text = text.replace("Vendorss","Vendors")
+#        text = text.replace("vendorssss","vendors")
+#        text = text.replace("vendorsss","vendors")
+#        text = text.replace("vendorss","vendors")
+#        text = text.replace("Vendorssss","Vendors")
+#        text = text.replace("Vendorsss","Vendors")
+#        text = text.replace("Vendorss","Vendors")
+        text = text.replace("Vendors","vendors")
         f.write(text)
         
 

0248e660f386832ad0f2f439dcc01c7f75e8d81a
Eric Schles <ericschles@gmail.com>
2016-10-09 23:11:34 -0400

    updating

--- vendors/management/commands/check_sam.py
+++ vendors/management/commands/check_sam.py
@@ -37,12 +37,10 @@ class Command(BaseCommand):
                 #keep from bringing the SAM API down
                 time.sleep(2)
                 #get SAM.gov API response for this Vendors
-                try:
-                    uri = settings.SAM_API_URL + v.duns_4 + '?api_key=' + settings.SAM_API_KEY
-                    log_uri = settings.SAM_API_URL + v.duns_4
-                except:
-                    import code
-                    code.interact(local=locals())
+                
+                uri = settings.SAM_API_URL + v.duns_4 + '?api_key=' + settings.SAM_API_KEY
+                log_uri = settings.SAM_API_URL + v.duns_4
+                
                 self.logger.debug("Fetching vendors at {0}".format(log_uri))
                 req = requests.get(uri)
                 

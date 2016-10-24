295837fa78dcf0ea861dcfc5f20e18e42924507f
Eric Schles <ericschles@gmail.com>
2016-10-23 21:02:56 -0400

    everything works locally, cannot push to server

--- mirage/settings.py
+++ mirage/settings.py
@@ -1,11 +1,7 @@
-import dj_database_url
-
 """
 Django settings for mirage project.
-
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
-
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
@@ -13,15 +9,16 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import markdown
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
+SECRET_KEY="this is totally a secret"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     "mirage.context_processors.api_host",
@@ -40,7 +37,7 @@ TEMPLATE_DIRS = (
 
 )
 ALLOWED_HOSTS = [
-    '.cloud.gov',
+    'discovery.gsa.gov',
     '.gsa.gov',
     '127.0.0.1', 
     '.18f.us',
@@ -84,9 +81,20 @@ WSGI_APPLICATION = 'mirage.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': {
+      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      'NAME': 'oasis',
+      'USER': 'oasis',
+      'PASSWORD': '1234'
+    }
+}
 
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
+SAM_API_KEY = os.getenv("OASIS_DISCOVERY_SAM_API_KEY")
+API_HOST = "https://api.data.gov/gsa/discovery/"
+API_KEY = os.getenv("OASIS_DISCOVERY_API_KEY")
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -192,7 +200,7 @@ SWAGGER_SETTINGS = {
     "exclude_namespaces": [], # List URL namespaces to ignore
     "api_version": '0.1',  # Specify your API's version
     "api_path": "/",  # Specify the path to your API not a root level
-    "api_host": '', #comment out until fix swagger - API_HOST, #the data.gov api host
+    #"api_host": '', #comment out until fix swagger - API_HOST, #the data.gov api host
     "enabled_methods": [  # Specify which methods to enable in Swagger UI
         'get',
     ],
@@ -207,14 +215,10 @@ SWAGGER_SETTINGS = {
 This API drives the [Discovery Market Research Tool](https://discovery.gsa.gov).
 It contains information on the vendors that are part of the OASIS and OASIS Small Business contracting vehicles, such as their contracting history, their elligibility for contract awards, and their small business designations.
 To learn more about the tool, please visit [Discovery](https://discovery.gsa.gov) or see the README on our [GitHub repository](https://github.com/18F/mirage).
-
 **Please note that the base path for this API is `https://api.data.gov/gsa/discovery/`**
-
 It requires an API key, obtainable at [api.data.gov](http://api.data.gov/).
 It must be passed in the `api_key` parameter with each request.
         """), #converts markdown description to HTML
     },
     "template_path": "api_theme/index.html",
 }
-
-
